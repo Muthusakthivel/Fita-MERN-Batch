d7a93ac9b3ee791047d9cc3ee520f4b299ae31ac
muthukumar <muthukumar.s@piquota.com>
2023-02-21 09:38:16 +0530

    useState

--- src/components/simple-form.js
+++ src/components/simple-form.js
@@ -7,6 +7,10 @@ const SimpleFormSubmition = () => {
     // let [variableName, functionName] = useState(initialValue);
 
     let [userName, setUserName] = useState("");
+    let [loginForm, setLoginForm] = useState({
+        username: '',
+        password: ''
+    });
 
     let message = "Hi there! Good Morning"
     // let userName;
@@ -19,27 +23,28 @@ const SimpleFormSubmition = () => {
     }
 
     const onSubmitForm = () => {
-        alert("Submitted successfully")
+        console.log(loginForm);
     };
 
     const onHandleInput = (event) => {
-        setUserName(event.target.value);
+        // setUserName(event.target.value);
         // console.log(userName);
         // userName = event.target.value;
         // console.log(event);
         // console.log(event.target.value);
         // console.log(userName);
         // console.log(event.target.value, event.target.id);
+        setLoginForm({...loginForm, [event.target.id] : event.target.value});
     };
     return(
      <div>
         {/* <h1>This is a simple Form</h1> */}
         <h1>{message}</h1>
         <h2>The UserName is {userName}</h2>
-        <h1>Student Info <h2>{student_information.name}</h2><h2>{student_information.course}</h2></h1>
+        <h1>Login Info <h2>{loginForm.username}</h2><h2>{loginForm.password}</h2></h1>
         <div className="space">
             <label className="label">Enter Your Email ID</label>
-            <input type='email' placeholder='Enter your email' name="email" id="userName"  onChange={onHandleInput} />
+            <input type='email' placeholder='Enter your email' name="email" id="username"  onChange={onHandleInput} />
         </div>
         <div className="space">
             <label className="label">Enter Your Password</label>
