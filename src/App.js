56dbd7ed4b2e9281fca8074401e88db8e80856de
muthukumar <muthukumar.s@piquota.com>
2023-03-13 09:30:05 +0530

    Put & Delete

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import InboxPage from './pages/inbox';
 import SentPage from './pages/sent';
 import TrashPage from './pages/trash';
 import AccountCreatePage from './pages/account-create';
+import UpdateUser from './pages/update-users';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         <Route path='sent' element={<SentPage/>}></Route>
         <Route path='trash' element={<TrashPage />}></Route>
         <Route path='account' element={<AccountCreatePage />}></Route>
+        <Route path='update' element={<UpdateUser />}></Route>
       </Route>
       <Route path='register' element={<RegisterPage />}></Route>
       <Route path='user' element={<UserPage/>}></Route>
--- src/pages/users.js
+++ src/pages/users.js
@@ -22,6 +22,20 @@ const UserPage = () => {
             console.log(e)
         })
     }
+    const deleteUser = (user) => {
+        console.log(user.id)
+
+        const url = "https://reqres.in/api/users/" + user.id
+
+        axios.delete(url)
+        .then((response) => {
+            console.log(response)
+            alert("Successfully Profile Has been deleted")
+        })
+        .catch((error) => {
+            console.log(error.message);
+        })
+    }
     return(
         <div>
             <h1>List of Users</h1>
@@ -33,6 +47,7 @@ const UserPage = () => {
                         <th>Name</th>
                         <th>Email ID</th>
                         <th>Profile Pic</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -46,6 +61,9 @@ const UserPage = () => {
                                     {/* <td>{value.avatar}</td> */}
                                     <td>
                                         <img src={value.avatar} alt="profile"/>
+                                    </td>
+                                    <td>
+                                        <img src={require('../images/delete.png')} alt="profile" style={{"width" : "25px", "cursor" : "pointer"}} onClick={() => deleteUser(value)}/>
                                     </td>
                                 </tr>
                             )
